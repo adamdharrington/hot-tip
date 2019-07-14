cec8e0cc9ef746acdef677f546b5b3a0aed5b907
Adam Harrington <adamdharrington@gmail.com>
2019-07-14 14:54:11 +0100

    Aria (#10)
    * [feature]: accessibility
    
    - Add appropriate ARIA tags
    - also support render props
    - tests and examples
    
    * [fix]: fix prerelease version ref

--- examples/gatsby-browser.js
+++ examples/gatsby-browser.js
@@ -1,1 +1,2 @@
 import 'hot-tip/dist/styles.scss'
+import 'font-awesome/css/font-awesome.min.css'
--- examples/gatsby-config.js
+++ examples/gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Hot Tip`,
+    title: 'Hot Tip',
+    author: 'Adam Harrington',
     description: `The stress-free tooltip solution.`,
+    lang: 'en-ie',
   },
   plugins: [`gatsby-plugin-sass`],
   pathPrefix: `/hot-tip`,
